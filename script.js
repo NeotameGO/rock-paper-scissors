3ddc6b84f92f21231183663ce692c43b71174e95
Neotame <117200516+NeotameGO@users.noreply.github.com>
2022-11-04 19:31:32 +0100

    Add game function with 5 round result

--- script.js
+++ script.js
@@ -12,20 +12,45 @@ function playerSelection(choice) {
 
 function playRound(computer, player) {
     if (computer == "Rock" && player == "Paper") {
-       return "You Lose! Paper beats Rock";
+       return "You Lose!";
     } else if (computer == "Rock" && player == "Scissors") {
-        return "You Lose! Rock beat Scissors";
+        return "You Lose!";
     } else if (computer == "Paper" && player == "Scissors") {
-        return "You Win! Scissors beat paper";
+        return "You Win!";
     } else if (computer == "Paper" && player == "Rock") {
-        return "You Lose! Paper beats Rock";
+        return "You Lose!";
     } else if (computer == "Scissors" && player == "Paper") {
-         return "You Lose! Scissors beat Paper";
+         return "You Lose!";
     } else if (computer == "Scissors" && player == "Rock") {
-        return "You Win! Rock beats Scissors";
+        return "You Win!";
     } else {
         return "Draw!"
     }
 }
 
-console.log(game(getComputerChoice(), playerSelection("rock")));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+   for (i = 0; i < 5; i++) {
+    let result = playRound(getComputerChoice(), playerSelection("rock"));
+    if (result == "You Win!") {
+        playerScore++;
+    } else if (result == "You Lose!") {
+        computerScore++;
+    } 
+
+    console.log(result);
+    console.log(playerScore);
+    console.log(computerScore);
+   }
+
+   if (playerScore > computerScore) {
+    console.log("You Win " + playerScore + " : " + computerScore);
+   } else if (playerScore < computerScore) {
+    console.log("You Lose " + playerScore + " : " + computerScore);
+   } else {
+    console.log("Draw " + playerScore + " : " + computerScore);
+   }
+}
+
+game();
