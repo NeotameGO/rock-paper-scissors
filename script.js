5357a375edcccfe3eec87fbccce0d9576d8d6821
Neotame <117200516+NeotameGO@users.noreply.github.com>
2022-11-04 18:30:26 +0100

    Add game function

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ function playerSelection(choice) {
     return choice;
 }
 
-console.log(playerSelection("paper"));
-console.log(getComputerChoice());
+function game(computer, player) {
+    if (computer == "Rock" && player == "Paper") {
+       return "You Lose! Paper beats Rock";
+    } else if (computer == "Rock" && player == "Scissors") {
+        return "You Lose! Rock beat Scissors";
+    } else if (computer == "Paper" && player == "Scissors") {
+        return "You Win! Scissors beat paper";
+    } else if (computer == "Paper" && player == "Rock") {
+        return "You Lose! Paper beats Rock";
+    } else if (computer == "Scissors" && player == "Paper") {
+         return "You Lose! Scissors beat Paper";
+    } else if (computer == "Scissors" && player == "Rock") {
+        return "You Win! Rock beats Scissors";
+    } else {
+        return "Draw!"
+    }
+}
+
+console.log(game(getComputerChoice(), playerSelection("Rock")));
